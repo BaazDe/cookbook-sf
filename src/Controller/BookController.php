8835a150993fc50d04ed9a4ed01eab9ecb78ef46
Valentin Desneaux <vdesneaux@gmail.com>
2020-01-30 23:41:07 +0100

    blocked. need help

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Book;
+use App\Entity\Recipe;
 use App\Form\BookType;
 use App\Repository\BookRepository;
 use App\Repository\RecipeRepository;
@@ -117,13 +118,42 @@ class BookController extends AbstractController
     /**
      * @param RecipeRepository $repository
      * @param Book $book
+     * @param Request $request
+     * @param Recipe $recipe
      * @return Response
      * @Route("/{id}/recipes", name="book_recipes")
      */
-    public function recipesFromBook(RecipeRepository $repository, Book $book): Response
+    public function recipesFromBook(RecipeRepository $repository, Book $book, Request $request): Response
     {
+        $recipe = new Recipe();
+        $form = $this->createForm(Recipe::class, $recipe);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $image =$form['mainImage']->getData();
+
+            if ($image) {
+                $imageName = md5(uniqid()). '.'.$image->guessExtension();
+                // Move the file to the directory where brochures are stored
+                $image->move($this->getParameter('upload_directory_book'), $imageName);
+
+            } else {
+                $imageName = "transparent.png";
+            }
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $recipe->setMainImage($imageName);
+
+            $entityManager->persist($recipe);
+            $entityManager->flush();
+
+            return $this->redirect($request->getRequestUri());
+        }
+
         return $this->render('recipe/recipes.html.twig', [
-            'recipes' => $repository->findBy(['book'=> $book])
+            'recipes' => $repository->findBy(['book'=> $book]),
+            'form' => $form->createView(),
         ]);
     }
 }
